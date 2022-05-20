a75f54d6a566787c22c33cf529f52625ec6388a4
amgs <amgs>
2022-05-20 17:33:48 +0100

    Add basic EJ logic - NOT DONE!

--- src/views/cli.c
+++ src/views/cli.c
@@ -6,9 +6,10 @@
 
 #include "cli.h"
 
-#include "../utils/str_utils.h"
-#include "../models/player.h"
 #include "../models/game.h"
+#include "../models/player.h"
+#include "../utils/list.h"
+#include "../utils/str_utils.h"
 
 void cli() {
     char* line = NULL;
@@ -25,19 +26,29 @@ void cli() {
         char* command = strtok(line, " ");
         if (strcmp(command, "RJ") == 0) {
             char* name = strtok(NULL, " ");
-            if(player_exists(game, name)) {
+            if (player_exists(game, name)) {
                 printf("Jogador existente.\n");
             } else {
                 add_player(game, name);
                 printf("Jogador registado com sucesso.\n");
             }
         } else if (strcmp(command, "LJ") == 0) {
-            Player *players = get_player_array(game);
+            Player* players = get_player_array(game);
             // TODO: Ordenar players.
             for (int i = 0; i < num_players(game); i++) {
                 printf("%s %d %d\n", players[i]->name, players[i]->games_played, players[i]->wins);
             }
             free(players);
+        } else if (strcmp(command, "EJ") == 0) {
+            char* name = strtok(NULL, " ");
+            // FIXME: NOT DONE!!
+            Player player = new_player(name);
+            int pos = list_find(game->players, (bool (*)(void*, void*))equal_players, player);
+            free(player);
+            player = list_get(game->players, pos);
+            list_remove(game->players, pos);
+            free(player);
+            printf("Jogador removido com sucesso.\n");
         }
     }
     free(line);
